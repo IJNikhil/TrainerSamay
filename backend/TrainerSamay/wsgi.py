94aab02be9966e9bdd0c71da67eeb6cf98153cab
Nikhileshwar Adam <nikhileshwar41@gmail.com>
2026-01-02 17:25:37 +0530

    Force-set DJANGO_SETTINGS_MODULE to TrainerSamay.settings

--- backend/TrainerSamay/wsgi.py
+++ backend/TrainerSamay/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainersamay.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'TrainerSamay.settings'
 
 application = get_wsgi_application()
